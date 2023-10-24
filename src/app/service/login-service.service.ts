50f19c9d6fcb4d3640c63cfe6f31014705c26d14
zJimpi <jimpidesk@gmail.com>
2023-10-24 21:58:50 +0530

    spelling reckeck

--- src/app/service/login-service.service.ts
+++ src/app/service/login-service.service.ts
@@ -10,6 +10,7 @@ export class LoginServiceService {
   loggedIn : boolean =false;
   adminIn : boolean = false;
   constructor(private _http:HttpClient) { }
+  
   adduser(data:any):Observable<any>{
     return this._http.post('http://localhost:3000/user',data);
   }
